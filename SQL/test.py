228b1efb18d2f387039448bb4a41456b326bfdb0
Mahi-23691 <23691@burnside.school.nz>
2025-04-29 17:37:07 +1200

    adding test.py from SQL tutorial

---
+++ SQL/test.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+DATABASE = 'C:\PersonalUse\School\Software Engineering\SQL\cars.db'
+
+def print_all_cars():
+    with sqlite3.connect(DATABASE) as db:
+
+        speed = input("What Speed: ")
+
+        cursor = db.cursor()
+        sql = 'SELECT * FROM Car WHERE top_speed > ?;'
+        cursor.execute(sql, (speed,))
+        results = cursor.fetchall()
+
+        #Print Propersly
+        for car in results:
+            print(f"Car: {car[1]} || Top Speed: {car[2]}")
+
+if __name__ == '__main__':
+    print_all_cars()
